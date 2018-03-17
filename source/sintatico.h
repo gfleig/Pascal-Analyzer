02ddeadaf60c1e1cc49da2ec125cd25ac691257f
Fleig <fleigalves@hotmail.com>
2018-03-17 15:32:41 -0300

    atualização

--- source/sintatico.h
+++ source/sintatico.h
@@ -17,3 +17,49 @@ int opMultiplicativo()
         return 1;
     }
 }
+
+int opAditivo()
+{
+    getSymbol();
+    if(currentToken.TokenType == "Op. Aditivo")
+    {
+        return 1;
+    }
+}
+
+int opRelacional()
+{
+    getSymbol();
+    if(currentToken.TokenType == "Op. Relacional")
+    {
+        return 1;
+    }
+}
+
+int sinal()
+{
+    getSymbol();
+    if(currentToken.TokenType == "Op. Relacional")
+    {
+        return 1;
+    }
+}
+
+int fator()
+{
+    getSymbol();
+    if( currentToken.TokenType == "Identificador" ||
+        currentToken.TokenType == "Inteiro" ||
+        currentToken.TokenType == "Real" ||
+        currentToken.symbol == "true" ||
+        currentToken.symbol == "false" ||
+    )
+    {
+        return 1;
+    }
+    else if (currentToken.symbol == "not")
+    {
+        return expressao();
+    }
+    else if
+}
